e659c0eaa0a6686d961618ea381b61642125e19d
OlexMakar <olexmakar@gmail.com>
2021-03-29 15:56:48 +0300

    Changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // let numbers = [25, -6, 39, -10, 12]
 // numbers.forEach((val, i, arr) => {
-//    if (arr[i] < 0) return arr[i] = 0  Коли використовується у forEach не треба return
+//    if (arr[i] < 0) return arr[i] = 0
 //    else
 //       return arr[i];
 // })
@@ -17,7 +17,8 @@
 // let enterPrise = parseFloat(prompt('Введіть ціну'));
 // let newPrises = prises.filter((val, i, arr) =>
 //    (val < enterPrise))
-// document.write(newPrises)
+// document.write(newPrises)dcdscds  
+
 
 //-----------------------------------------------------
 
@@ -26,7 +27,7 @@
 
 // let prises = [490, 1250, 150, 720, 2600, 1100]
 // let newArray = prises.map((val, i, arr) => {
-//    if (val < 1000) return (val *= 1.20).toFixed(2)   <-- Ок. Але тут більше підходило  forEach
+//    if (val < 1000) return (val *= 1.20).toFixed(2)
 //    if (val > 1000) return (val *= 0.70).toFixed(2)
 // })
 // document.write(newArray)
@@ -36,11 +37,72 @@
 // Задача 4. Дано масив імен. Сформувати новий масив, який складається з імен, 
 // які починаються на задану літеру.
 
-let names = ['Alla', 'Anya', 'Vova', 'Vika', 'Serhiy', 'Yana', 'Yura', 'Oksana',
-   'Igor', 'Anatoliy', 'Diana']
-let symbol = prompt('Введіть першу букву імені (англійська велика літера)')
-let newArray = names.filter((val, i, arr) => {
-   return val[0] === symbol
-})
-document.write(newArray)
+// let names = ['Alla', 'Anya', 'Vova', 'Vika', 'Serhiy', 'Yana', 'Yura', 'Oksana',
+//    'Igor', 'Anatoliy', 'Diana']
+// let symbol = prompt('Введіть першу букву імені (англійська велика літера)')
+// let newArray = names.filter((val, i, arr) => {
+//    return val[0] === symbol
+// })
+// document.write(newArray)
 
+//Задача 5.Задача 5. Дано ваги автомобілів на парковці. З’ясувати, чи усі автомобілі 
+//є легковими (легшими за 2000 кг).
+
+// let carsWeight = [1500, 1300, 5200, 2500, 1200]
+// let x = carsWeight.every((val, i, arr) => val < 2000)
+// if (x) document.write('Всі авто є легковими')
+// else
+//    document.write('Не всі авто є легковими')
+
+//Задача 6. Дано масив, який зберігає кількість відвідувачів протягом тижня. З’ясувати, 
+//чи були дні, коли кількість відвідувачів була меншою за 10.
+
+// let visitors = [12, 15, 13, 14, 9, 5, 4]
+// if (visitors.some((val, i, arr) =>
+//    val < 10))
+//    alert('Були такі дні')
+// else ('Не було таких днів')
+
+
+//Задача 7. Відсортувати масив імен за останньою літерою.
+// let names = ['Alla', 'Anya', 'Vova', 'Vika', 'Serhiy', 'Yana', 'Yura', 'Oksana',
+//    'Igor', 'Anatoliy', 'Diana']
+// let sorted = names.sort(function (a, b) {
+
+//    if (a[a.length - 1] > b[b.length - 1])
+//       return 1;
+//    else if (a[a.length - 1] < b[b.length - 1])
+//       return -1;
+//    return 0;
+// })
+
+// alert(sorted)
+
+// Задача 8. Дано масив цін. Для усіх товарів, які дорожчі за 1000грн дати знижку 20% 
+// (помножити на 0.8), а для усіх інших товарів – надати 5 % знижки (помножити на 0.95).
+
+// let prises = [1200, 900, 327, 1690, 830, 1050]
+// prises.forEach((val, i, arr) => {
+//    if (arr[i] > 1000) return arr[i] *= 0.8
+//    else return arr[i] *= 0.95
+// })
+// document.write(prises)
+
+// Задача 9. Дано масив років народження працівників. Вивести масив тих, які є призовного віку 
+// (менше 25 років).
+
+// let ages = [1982, 1991, 1998, 2000, 1994, 2001]
+// let conscript = ages.filter((val, i, arr) => 2021 - val < 25)
+// alert(conscript)
+
+// Задача 10. Дано масив показників гемоглобіну (масив чисел). Сформувати масив тих, які 
+// є меншими за 115.
+
+// let hemohlobin = [125, 110, 137, 145, 114, 150, 105]
+// let until115 = hemohlobin.filter((val, i, arr) => val < 115)
+// document.write(until115)
+
+// //Задача 11. Дано масив цін товарів. Сформувати масив тих, які є меншими за 2000грн.
+// let prises = [1200, 2300, 1800, 3200, 5300, 1000]
+// let newPrises = prises.filter((val, i, arr) => val < 2000)
+// document.write(newPrises)
